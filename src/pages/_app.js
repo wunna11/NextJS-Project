418ae28f4b9f94b54f8bb41c148bbcd2c200fd2a
wunna11 <wana11391@gmail.com>
2023-11-23 22:33:30 +0630

    api route

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,8 @@
-import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
       <Component {...pageProps} />
-    </Layout>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,65 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import { useRef, useState } from 'react';
 
-function HomePage(props) {
-  // const featuredEvents = getFeaturedEvents();
-  console.log('events', props.events);
+function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+    console.log('event', reqBody);
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler() {
+    fetch('/api/feedback')
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
+  }
 
   return (
     <div>
-      <EventList items={props.events} />
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <div key={item.id}>
+            <li>{item.email}</li>
+            <li>{item.text}</li>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default HomePage;
-
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      events: featuredEvents,
-    },
-  };
-}
